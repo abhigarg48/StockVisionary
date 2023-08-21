ad3cd612955f33e9b3523a5bc7f99200015dbf44
Abhishek Garg <93115017+abhigarg48@users.noreply.github.com>
2023-08-21 19:49:50 +0530

    Create DataCollection.py
    Installing Libraries for DataUse.

---
+++ DataCollection.py
@@ -0,0 +1,16 @@
+# Obtain 'API key' https://www.alphavantage.co/ 
+ 
+# Installing Libraries (cmd)
+pip install pandas
+pip install numpy 
+pip install scikit-learn
+pip install tensorflow 
+pip install alpha_vantage
+pip install matplotlib
+
+#Else Shortcut Installing Libraries (cmd)
+pip install pandas numpy scikit-learn tensorflow alpha_vantage matplotlib
+
+#Verify Install
+pip show 'library name'
+
