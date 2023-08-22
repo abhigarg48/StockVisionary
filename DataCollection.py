0552ba7a355ea6d5136dee7de61493dad73c0fcd
Abhishek Garg <93115017+abhigarg48@users.noreply.github.com>
2023-08-22 22:46:58 +0530

    Update DataCollection.py
    Importing TimeSeries for historical data.

--- DataCollection.py
+++ DataCollection.py
@@ -1,5 +1,6 @@
 # Obtain 'API key' https://www.alphavantage.co/ 
- 
+
+
 # Installing Libraries (cmd)
 pip install pandas
 pip install numpy 
@@ -11,6 +12,23 @@ pip install matplotlib
 #Else Shortcut Installing Libraries (cmd)
 pip install pandas numpy scikit-learn tensorflow alpha_vantage matplotlib
 
+
 #Verify Install
 pip show 'library name'
 
+
+#Data Retrieving
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+
+
+# Set your Alpha Vantage API key and stock symbol
+api_key = 'API-Key'
+symbol = 'AAPL' #Apple Stock Data
+
+# Initialize Alpha Vantage API client
+ts = TimeSeries(key=api_key, output_format='pandas')
+
+# Retrieve historical daily stock price data for the past 30 years
+data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+data = data.tail(30*252)  # Consider approximately 252 trading days per year
