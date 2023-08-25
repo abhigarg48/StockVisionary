8e29be1279601a510b783a84fbdf090452942495
Abhishek Garg <93115017+abhigarg48@users.noreply.github.com>
2023-08-25 21:23:41 +0530

    Create AutomateScheduler.py

---
+++ AutomateScheduler.py
@@ -0,0 +1,7 @@
+import schedule
+import time
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
