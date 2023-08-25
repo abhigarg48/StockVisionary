589512c4627feb7a8d2beddb228c3a4aecd9727f
Abhishek Garg <93115017+abhigarg48@users.noreply.github.com>
2023-08-25 21:21:32 +0530

    Create DataClean_Main.py

---
+++ DataClean_Main.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Define the file path for the CSV file
+file_path = os.path.join('AAPL_Stock_data.csv')
+
+# Load CSV file
+data = pd.read_csv(file_path)
+
+# Drop duplicates and missing values
+data.drop_duplicates(inplace=True)
+data.dropna(inplace=True)
+
+# Feature selection: Use relevant columns (adjust as needed)
+features = ['Open', 'High', 'Low', 'Close', 'Volume']
+X = data[features]
+
+# Target variable: Use 'Close' price
+y = data['Close']
